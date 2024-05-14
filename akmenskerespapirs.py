31a1d52971009c1283c386972403a30ee6ee243e
23DP4RPilans <rpilans007@gmail.com>
2024-05-14 09:34:08 +0300

    Changed: -Location of player_choice Added: -variables that count game outcomes -for loops to run code 5 times in a row -wins, losses, draws, invalid_choices variables -time module for time.sleep functions -Print results at end with statistics for last 5 games with an extra invalid choices printed if such messages entered

--- akmenskerespapirs.py
+++ akmenskerespapirs.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 rock = '''
         _______
@@ -42,66 +42,108 @@ loss = '''  _    ___  ___ ___
                     '''
 
 
-player_choice = input("Play the game: rock, scissors, paper! Choose your element by typing - rock, paper or scissors. > ")
 choices = ["rock", "scissors", "paper"]
 chosen_action = {rock: "rock", scissors: "scissors", paper: "paper"}
 computer_choice = random.choice(choices)
+games = 0
+wins = 0
+losses = 0
+draws = 0
+invalid_choices = 0
+last5results = []
 
 
-    
+for i in range(5):
+    player_choice = input('''--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                          Play the game: rock, paper, scissors! Choose your element by typing - rock, paper or scissors: ''')    
+    time.sleep(0.25)
+    print("User chose:", '''
+      O O
+       -
+    \\_____/  
+    ''', end = '')
+    if player_choice == "rock":
+        print(rock)
+    elif player_choice == "paper":
+        print(paper)
+    elif player_choice == "scissors":
+        print(scissors)
+    elif player_choice != choices:
+        invalid_choices = invalid_choices + 1
+        print("???")
+    print(f"{player_choice}")
 
-print("User chose:", '''
-  O O
-   -
-\\_____/  
-''', end = '')
-if player_choice == "rock":
-    print(rock)
-elif player_choice == "paper":
-    print(paper)
-elif player_choice == "scissors":
-    print(scissors)
-elif player_choice != choices:
-    print("???")
-print(f"{player_choice}")
+    time.sleep(1.5)
+    print("Computer chose:", '''
+       -------
+     /        \\
+    |  O   O  |
+    |         |   
+     \\  ---   /
+       -------
+    ''', end = '')
+    if computer_choice == "rock":
+        print(rock)
+    elif computer_choice == "paper":
+        print(paper)
+    elif computer_choice == "scissors":
+        print(scissors)
+    print(f"{computer_choice}")
+    time.sleep(1.5)
+        
+    if player_choice == "rock" and computer_choice == "scissors":
+        games = games + 1
+        wins = wins + 1
+        last5results.append("Win")
+        print(win)
+    elif player_choice == "paper" and computer_choice == "rock":
+        games = games + 1
+        wins = wins + 1
+        last5results.append("Win")
+        print(win)
+    elif player_choice == "scissors" and computer_choice == "paper":
+        games = games + 1
+        wins = wins + 1
+        last5results.append("Win")
+        print(win)
 
 
-print("Computer chose:", '''
-  -------
- /        \\
-|  O   O  |
-|         |   
- \\  ---  /
-  -------
-''', end = '')
-if computer_choice == "rock":
-    print(rock)
-elif computer_choice == "paper":
-    print(paper)
-elif computer_choice == "scissors":
-    print(scissors)
-print(f"{computer_choice}")
+    if player_choice == "rock" and computer_choice == "paper":
+        games = games + 1
+        losses = losses + 1
+        last5results.append("Loss")
+        print(loss)
+    elif player_choice == "paper" and computer_choice == "scissors":
+        games = games + 1
+        losses = losses + 1
+        last5results.append("Loss")
+        print(loss)
+    elif player_choice == "scissors" and computer_choice == "rock":
+        games = games + 1
+        losses = losses + 1
+        last5results.append("Loss")
+        print(loss)
 
-
-if player_choice == "rock" and computer_choice == "rock":
-    print(draw)
-elif player_choice == "paper" and computer_choice == "paper":
-    print(draw)
-elif player_choice == "scissors" and computer_choice == "scissors":
-    print(draw)
-
-    
-if player_choice == "rock" and computer_choice == "scissors":
-    print(win)
-elif player_choice == "paper" and computer_choice == "rock":
-    print(win)
-elif player_choice == "scissors" and computer_choice == "paper":
-    print(win)
-
-
-if player_choice == "rock" and computer_choice == "paper":
-    print(loss)
-elif player_choice == "paper" and computer_choice == "scissors":
-    print(loss)
-elif player_choice == "scissors" and computer_choice == "rock":
-    print(loss)
+    if player_choice == "rock" and computer_choice == "rock":
+        games = games + 1
+        draws = draws + 1
+        last5results.append("Draw")
+        print(draw)
+    elif player_choice == "paper" and computer_choice == "paper":
+        games = games + 1
+        draws = draws + 1
+        last5results.append("Draw")
+        print(draw)
+    elif player_choice == "scissors" and computer_choice == "scissors":
+        games = games + 1
+        draws = draws + 1
+        last5results.append("Draw")
+        print(draw)
+print('''-------------------------------------------
+      Restults from the last 5 games: ''')
+print()
+print("Wins: ", wins)
+print("Losses: ", losses)
+print("Draws: ", draws)
+if invalid_choices > 0:
+    print("Invalid choices: ", invalid_choices)
